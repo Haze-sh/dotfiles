85c746812cd30e0cd4591853104dcfe70a216bae
Haze-sh <hazemibnessam@gmail.com>
2022-11-16 05:26:53 +0200

    HPI changes

--- private_dot_config/private_my/my/config/__init__.py
+++ private_dot_config/private_my/my/config/__init__.py
@@ -1,4 +1,11 @@
 from my.core import Paths, PathIsh, get_files
 
+class location:
+    class gpslogger:
+        export_path: Paths = '~/Documents/Notes/Personal/Journal/Collections/References/Places'
+        accuracy: float = 50.0
+
+# Add timezone class later
+
 class hypothesis:
     export_path: Paths = '~/Documents/Notes/Personal/Journal/Collections/Annotations/hyp-annotations.json'
