1b50fb1a105a6801bb04802f5f044fbc52ab4c54
Haze-sh <hazemibnessam@gmail.com>
2022-12-02 08:43:13 +0200

    Music collection

---
+++ private_dot_config/platypush/scripts/music/sync.py
@@ -0,0 +1,38 @@
+from logging import getLogger
+
+from platypush.context import get_plugin
+from platypush.event.hook import hook
+from platypush.message.event.music import NewPlayingTrackEvent
+
+logger = getLogger('music_sync')
+
+# SQLAlchemy connection string that points to your database
+music_db_engine = 'postgresql+pg5432://electron@localhost/music'
+
+
+# Hook that react to NewPlayingTrackEvent events
+@hook(NewPlayingTrackEvent)
+def on_new_track_playing(event, **_):
+    track = event.track
+
+    # Skip if the track has no artist/title specified
+    if not (track.get('artist') and track.get('title')):
+        return
+
+    logger.info(
+        'Inserting track: %s - %s',
+        track['artist'], track['title']
+    )
+
+    db = get_plugin('db')
+    db.insert(
+        engine=music_db_engine,
+        table='tmp_music',
+        records=[
+            {
+                'artist': track['artist'],
+                'title': track['title'],
+                'album': track.get('album'),
+            }
+        ]
+    )
