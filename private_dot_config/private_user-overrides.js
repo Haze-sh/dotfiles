c91cd60aaa5ffbf2968e6867bb146210e1602009
Haze-sh <hazemibnessam@gmail.com>
2022-12-12 20:43:37 +0200

    Major mods

--- private_dot_config/private_user-overrides.js
+++ private_dot_config/private_user-overrides.js
@@ -1,4 +1,4 @@
-user_pref("signon.rememberSignons", false); 										// 5003
-user_pref("privacy.clearOnShutdown.history", false); 						// 2811
 user_pref("keyword.enabled", true); 														// 0801
 user_pref("layout.css.visited_links_enabled", false); 					// 0820
+user_pref("privacy.clearOnShutdown.history", false); 						// 2811
+user_pref("signon.rememberSignons", false); 										// 5003
