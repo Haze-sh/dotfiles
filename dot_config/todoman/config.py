5e835df96dce8b6a22c4f8da9bdfd93838e2d461
Haze-sh <hazemibnessam@gmail.com>
2023-06-05 05:46:59 +0300

    Major update II

---
+++ dot_config/todoman/config.py
@@ -0,0 +1,10 @@
+# A glob expression which matches all directories relevant.
+# path = "~/.local/share/calendars/*"
+path = "~/.local/share/calendars/todoman/18522a2b-67d0-12b7-7b3f-364ad8e7ce1d"
+date_format = "%Y-%m-%d"
+time_format = "%H:%M"
+default_list = "18522a2b-67d0-12b7-7b3f-364ad8e7ce1d"
+default_priority = 1
+default_due = 0
+humanize = True
+color = "always"
