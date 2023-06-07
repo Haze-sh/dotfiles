2f1e9d1c8f3992a6edb9048978d346e74d64c3e6
Haze-sh <hazemibnessam@gmail.com>
2023-06-07 15:34:26 +0300

    Port neovim to Lua

---
+++ private_dot_config/nvim/lua/custom/configs/lspconfig.lua
@@ -0,0 +1,4 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
---
+++ private_dot_config/nvim/lua/custom/plugins.lua
@@ -0,0 +1,13 @@
+local plugins = {
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end
+  },
+  {
+    "haze-sh/todo.txt-vim"
+  }
+}
+return plugins
