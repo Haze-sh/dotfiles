e0ba1d34a5e2a0e7a0a656cecdd9927aa16bcfe7
Haze-sh <hazemibnessam@gmail.com>
2023-06-10 09:07:13 +0300

    Updates

--- private_dot_config/nvim/lua/custom/chadrc.lua
+++
@@ -1,4 +0,0 @@
----@type ChadrcConfig 
- local M = {}
- M.ui = {theme = 'nord'}
- return M
--- private_dot_config/nvim/lua/custom/configs/lspconfig.lua
+++
@@ -1,4 +0,0 @@
-local on_attach = require("plugins.configs.lspconfig").on_attach
-local capabilities = require("plugins.configs.lspconfig").capabilities
-
-local lspconfig = require "lspconfig"
