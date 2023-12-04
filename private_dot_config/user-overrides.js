a9e1d198a47eee25ef0533a195951eece883368f
Haze-sh <hazemibnessam@gmail.com>
2023-12-04 16:35:56 +0200

    Various updates

--- private_dot_config/user-overrides.js
+++ private_dot_config/user-overrides.js
@@ -1,3 +1,5 @@
+user_pref("browser.startup.homepage", "about:home");            // 0103
+user_pref("browser.newtabpage.enabled", true);                  // 0104
 user_pref("keyword.enabled", true); 														// 0801
 user_pref("layout.css.visited_links_enabled", false); 					// 0820
 user_pref("privacy.clearOnShutdown.history", false); 						// 2811
