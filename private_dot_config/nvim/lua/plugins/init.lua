8a72a0c45f975743066ebb618b402d19f4b89d82
Haze-sh <hazemibnessam@gmail.com>
2025-10-25 00:07:59 +0300

    Great pump 🦾

--- private_dot_config/nvim/lua/plugins/init.lua
+++ private_dot_config/nvim/lua/plugins/init.lua
@@ -150,24 +150,6 @@ return {
     },
     config = function ()
       require("codecompanion").setup({
-        adapters = {
-          llama3 = function()
-            return require("codecompanion.adapters").extend("ollama", {
-              name = "llama3", -- Ensure the model is differentiated from Ollama
-              schema = {
-                model = {
-                  default = "llama3:latest",
-                },
-                num_ctx = {
-                  default = 16384,
-                },
-                num_predict = {
-                  default = -1,
-                },
-              },
-            })
-          end,
-        },
         strategies = {
           chat = { adapter = "ollama" },
           inline = { adapter = "ollama" },
